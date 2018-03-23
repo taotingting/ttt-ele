349d61fd22a0974ed3ff3418ddea5848a2d1f58d
陶婷婷 <taott@rayeye.cn>
2018-03-23 21:46:12 +0800

    Merge branch 'master' of https://github.com/taotingting/ttt-ele

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <header></header>
+    <div class="tab">
+        <div class="tab-item">
+          <router-link to="/goods">商品</router-link >
+        </div>
+       <div class="tab-item">
+          <router-link to="/ratings">评价</router-link >
+        </div>       
+        <div class="tab-item">
+          <router-link to="/seller">商家</router-link >
+        </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import header from './components/header/header.vue';
+
+export default{
+  components: {
+    'v-header': header
+  }
+  // data(){
+  //   return {
+
+  //   }
+  // }
+}
+</script>
+
+<style lang='less'>
+  .tab{
+    display: flex;
+    width:100%;
+    height: 40px;
+    line-height: 40px;
+    .tab-item{
+      flex:1;
+      text-align: center;
+      a{
+        display:block;
+        font-size: 14px;
+        color: rgb(77,85,93);
+        &.active{
+          color: rgb(240,20,20);
+        }
+      }
+    }
+  }
+</style>
